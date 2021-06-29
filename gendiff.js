1938cf98bdb03dd0a43800492ad43cc57cd7da2d
ArtemMalafeev <ma.evgenyevich@gmail.com>
2021-06-29 11:41:11 +0300

    add args and format

--- gendiff.js
+++ gendiff.js
@@ -6,7 +6,9 @@ const program = new Command();
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('0.1.0');
+  .version('0.1.0')
+  .option('-f, --format [type]', 'output format')
+  .arguments('<filepath1> <filepath2>');
 
 program.helpOption('-h, --help', 'output usage information');
 
