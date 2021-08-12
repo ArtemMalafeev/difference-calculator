f00f87c1aebe312da81e1c5e43ba44715187ffa1
ArtemMalafeev <ma.evgenyevich@gmail.com>
2021-08-12 14:44:13 +0300

    fix lint

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,7 +1,8 @@
-import genDiff from '../index.js';
-
 import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
+import { test, expect } from '@jest/globals';
+
+import genDiff from '../index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -12,14 +13,14 @@ test('diffJSON', () => {
   const file1 = getFixturePath('file1.json');
   const file2 = getFixturePath('file2.json');
 
-  expect(genDiff(file1, file2)).toEqual('{\n' +
-      '  - follow: false\n' +
-      '    host: hexlet.io\n' +
-      '  - proxy: 123.234.53.22\n' +
-      '  - timeout: 50\n' +
-      '  + timeout: 20\n' +
-      '  + verbose: true \n' +
-      '}');
+  expect(genDiff(file1, file2)).toEqual(`{
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true 
+}`);
 });
 
 test('incorrect file path', () => {
@@ -27,5 +28,5 @@ test('incorrect file path', () => {
   const file2 = getFixturePath('file2.json');
 
   expect(genDiff(file1, file2))
-      .toEqual(`incorrect ${file1}`);
+    .toEqual(`incorrect ${file1}`);
 });
