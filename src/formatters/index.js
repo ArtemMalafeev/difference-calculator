269f6788174a04d9d5f08bb0aa36875072a6290d
ArtemMalafeev <ma.evgenyevich@gmail.com>
2021-08-24 14:46:03 +0300

    add json fomatter

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -2,10 +2,12 @@ import _ from 'lodash';
 
 import stylish from './stylish.js';
 import plain from './plain.js';
+import json from './json.js';
 
 const formatters = {
   stylish,
   plain,
+  json,
 };
 
 export default (ast, format) => {
