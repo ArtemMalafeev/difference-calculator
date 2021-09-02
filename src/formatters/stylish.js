086d1448ffd3b7f75ed173cd9b75da5c12d2abbf
ArtemMalafeev <ma.evgenyevich@gmail.com>
2021-09-02 08:58:42 +0300

    fix indent

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -2,18 +2,18 @@ import _ from 'lodash';
 
 const indentSymbol = ' ';
 const indentSize = 2;
-const baseIndentSize = 2;
+const baseIndentSize = 4;
 
 const makeIndent = (depth) => `${indentSymbol.repeat(depth)}`;
 
 const stringify = (data, depth) => {
   if (_.isPlainObject(data)) {
-    const currentIndent = makeIndent(depth * indentSize + baseIndentSize);
-    const closeIndent = makeIndent(depth * indentSize - baseIndentSize);
+    const currentIndent = makeIndent(depth * baseIndentSize + indentSize);
+    const closeIndent = makeIndent(depth * baseIndentSize);
 
     const node = Object
       .entries(data)
-      .map(([key, value]) => `${currentIndent}${key}: ${stringify(value, depth + 2)}`);
+      .map(([key, value]) => `${currentIndent}  ${key}: ${stringify(value, depth + 1)}`);
 
     return ['{', ...node, `${closeIndent}}`]
       .join('\n');
@@ -26,13 +26,13 @@ const format = (node, depth) => {
   const {
     key, value, type, oldValue, newValue,
   } = node;
-  const currentIndent = makeIndent(depth * indentSize);
+  const currentIndent = makeIndent(depth * baseIndentSize + indentSize);
 
-  const addedType = () => `${currentIndent}+ ${key}: ${stringify(value, depth + 2)}`;
-  const removedType = () => `${currentIndent}- ${key}: ${stringify(value, depth + 2)}`;
+  const addedType = () => `${currentIndent}+ ${key}: ${stringify(value, depth + 1)}`;
+  const removedType = () => `${currentIndent}- ${key}: ${stringify(value, depth + 1)}`;
   const changedType = () => ([
-    `${currentIndent}- ${key}: ${stringify(oldValue, depth + 2)}`,
-    `${currentIndent}+ ${key}: ${stringify(newValue, depth + 2)}`,
+    `${currentIndent}- ${key}: ${stringify(oldValue, depth + 1)}`,
+    `${currentIndent}+ ${key}: ${stringify(newValue, depth + 1)}`,
   ]
     .join('\n')
   );
@@ -53,22 +53,22 @@ const format = (node, depth) => {
 };
 
 const build = (astTree) => {
-  const iter = (innerAst, depth = 1) => {
+  const iter = (innerAst, depth = 0) => {
     const result = innerAst
       .map((node) => {
         const { key, children, type } = node;
 
         if (type === 'nested') {
-          const inner = iter(children, depth + 2);
-          const currentIndent = makeIndent(depth * indentSize + baseIndentSize);
+          const inner = iter(children, depth + 1);
+          const currentIndent = makeIndent(depth * baseIndentSize + indentSize);
 
-          return `${currentIndent}${key}: ${inner}`;
+          return `${currentIndent}  ${key}: ${inner}`;
         }
 
         return format(node, depth);
       });
 
-    const closeIndent = makeIndent(depth * indentSize - baseIndentSize);
+    const closeIndent = makeIndent(depth * baseIndentSize);
 
     return ['{', ...result, `${closeIndent}}`]
       .join('\n');
